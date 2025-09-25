f559411883c5bbd6bf2a68e47761634322649970
Avaneesh Jambhale <90239357+MrGrappleMan@users.noreply.github.com>
2025-09-26 02:25:29 +0530

    Update main.zsh

--- main.zsh
+++ main.zsh
@@ -1,12 +1,17 @@
 #!/bin/zsh
 
+## Variables
 variable dfwrt = "defaults write"
 
 dfwrt /Library/Preferences/com.apple.loginwindow DesktopPicture ""
+
 dfwrt com.apple.LaunchServices LSQuarantine -bool false
+
+# keep disk integrity
 dfwrt com.apple.frameworks.diskimages skip-verify -bool false
 dfwrt com.apple.frameworks.diskimages skip-verify-locked -bool false
 dfwrt com.apple.frameworks.diskimages skip-verify-remote -bool false
+
 dfwrt -g NSAutomaticSpellingCorrectionEnabled -bool true
 dfwrt NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
 dfwrt NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
@@ -18,6 +23,11 @@ dfwrt com.apple.Finder ShowMountedServersOnDesktop -bool true
 dfwrt com.apple.Finder WarnOnEmptyTrash -bool true
 dfwrt com.apple.finder "FXEnableExtensionChangeWarning" -bool false
 dfwrt /Library/Preferences/com.apple.mDNSResponder.plist NoMulticastAdvertisements -bool NO
+
+# Keep backups
 tmutil enable
+
+# Homebrew Telemtry
 brew analytics on
+
 pmset -b sleep 2
