75fab6db8d8f6ff7969f9c1226935c7cc0726ff5
Avaneesh Jambhale <90239357+MrGrappleMan@users.noreply.github.com>
2025-09-27 21:42:11 +0530

    Update main.zsh

--- main.zsh
+++ main.zsh
@@ -11,11 +11,12 @@ tmutil enable
 dfwrt com.apple.Finder WarnOnEmptyTrash -bool true
 
 # Font Smoothing, makes text viewing comfortable
-defaults write -g CGFontRenderingFontSmoothingDisabled -bool NO
+dfwrt -g CGFontRenderingFontSmoothingDisabled -bool NO
 defaults -currentHost write -globalDomain AppleFontSmoothing -int 3
 
 # Productivity
-dfwrt -g NSAutomaticSpellingCorrectionEnabled -bool true
+dfwrt com.apple.finder "FXEnableExtensionChangeWarning" -bool "false"
+dfwrt -g NSAutomaticSpellingCorrectionEnabled -bool false
 dfwrt NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
 dfwrt NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
 dfwrt com.apple.Safari HomePage -string "https://scidsg.github.io/relaylove/"
@@ -30,15 +31,18 @@ dfwrt /Library/Preferences/com.apple.mDNSResponder.plist NoMulticastAdvertisemen
 launchctl load -w /System/Library/LaunchAgents/com.apple.ReportCrash.plist
 sudo launchctl load -w /System/Library/LaunchDaemons/com.apple.ReportCrash.Root.plist
 brew analytics on
+defaults write com.apple.appleseed.FeedbackAssistant Autogather -bool true
 
 # ON Security
-defaults write com.apple.LaunchServices LSQuarantine -bool true
+dfwrt com.apple.LaunchServices LSQuarantine -bool true
+defaults write com.apple.Safari WarnAboutFraudulentWebsites -bool true
 
+# Energy
 pmset -b sleep 2
 
 # ON HiDPI, better external screen resolution
-sudo defaults write /Library/Preferences/com.apple.windowserver.plist DisplayResolutionEnabled -bool true
+sudo dfwrt /Library/Preferences/com.apple.windowserver.plist DisplayResolutionEnabled -bool true
 
 # Bluetooth Optimizations, better bluetooth experience
-defaults write bluetoothaudiod "Enable AptX codec" -bool true
-defaults write bluetoothaudiod "Enable AAC codec" -bool true
+dfwrt bluetoothaudiod "Enable AptX codec" -bool true
+dfwrt bluetoothaudiod "Enable AAC codec" -bool true
