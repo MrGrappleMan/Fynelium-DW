bdc5eaf0ff38df195c596a0e22f432105feac9c1
Avaneesh Jambhale <90239357+MrGrappleMan@users.noreply.github.com>
2025-09-27 21:36:34 +0530

    Update main.zsh

--- main.zsh
+++ main.zsh
@@ -3,20 +3,18 @@
 ## Variables
 variable dfwrt = "defaults write"
 
-dfwrt /Library/Preferences/com.apple.loginwindow DesktopPicture ""
-
-dfwrt com.apple.LaunchServices LSQuarantine -bool false
-
-# ON Disk integrity
+# Integrity measures, prevents corruption and data loss
 dfwrt com.apple.frameworks.diskimages skip-verify -bool false
 dfwrt com.apple.frameworks.diskimages skip-verify-locked -bool false
 dfwrt com.apple.frameworks.diskimages skip-verify-remote -bool false
+tmutil enable
+dfwrt com.apple.Finder WarnOnEmptyTrash -bool true
 
-# Font Smoothing
+# Font Smoothing, makes text viewing comfortable
 defaults write -g CGFontRenderingFontSmoothingDisabled -bool NO
-defaults -currentHost write -globalDomain AppleFontSmoothing -int 2
+defaults -currentHost write -globalDomain AppleFontSmoothing -int 3
 
-# Finder
+# Productivity
 dfwrt -g NSAutomaticSpellingCorrectionEnabled -bool true
 dfwrt NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
 dfwrt NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
@@ -25,25 +23,22 @@ dfwrt com.apple.Finder ShowHardDrivesOnDesktop -bool true
 dfwrt com.apple.Finder ShowExternalHardDrivesOnDesktop -bool true
 dfwrt com.apple.Finder ShowRemovableMediaOnDesktop -bool true
 dfwrt com.apple.Finder ShowMountedServersOnDesktop -bool true
-dfwrt com.apple.Finder WarnOnEmptyTrash -bool true
 dfwrt com.apple.finder "FXEnableExtensionChangeWarning" -bool false
 dfwrt /Library/Preferences/com.apple.mDNSResponder.plist NoMulticastAdvertisements -bool NO
 
-# ON TimeMachine backups
-tmutil enable
-
-# ON Homebrew Telemetry
+# ON Telemetry
+launchctl load -w /System/Library/LaunchAgents/com.apple.ReportCrash.plist
+sudo launchctl load -w /System/Library/LaunchDaemons/com.apple.ReportCrash.Root.plist
 brew analytics on
 
-# ON ReportCrash
-launchctl unload -w /System/Library/LaunchAgents/com.apple.ReportCrash.plist
-sudo launchctl unload -w /System/Library/LaunchDaemons/com.apple.ReportCrash.Root.plist
+# ON Security
+defaults write com.apple.LaunchServices LSQuarantine -bool true
 
 pmset -b sleep 2
 
-# ON HiDPI
+# ON HiDPI, better external screen resolution
 sudo defaults write /Library/Preferences/com.apple.windowserver.plist DisplayResolutionEnabled -bool true
 
-# Bluetooth
+# Bluetooth Optimizations, better bluetooth experience
 defaults write bluetoothaudiod "Enable AptX codec" -bool true
 defaults write bluetoothaudiod "Enable AAC codec" -bool true
