3aa24aeb43b0b7c6495e85bdef7ba3daae41b045
Avaneesh Jambhale <90239357+MrGrappleMan@users.noreply.github.com>
2025-09-27 21:59:31 +0530

    Update main.zsh

--- main.zsh
+++ main.zsh
@@ -1,21 +1,23 @@
 #!/bin/zsh
 
 ## Variables
-variable dfwrt = "defaults write"
+variable dfwrt = "dfwrt"
 
 # Integrity measures, prevents corruption and data loss
 dfwrt com.apple.frameworks.diskimages skip-verify -bool false
 dfwrt com.apple.frameworks.diskimages skip-verify-locked -bool false
 dfwrt com.apple.frameworks.diskimages skip-verify-remote -bool false
-tmutil enable
-dfwrt com.apple.Finder WarnOnEmptyTrash -bool true
+tmutil enable # Allow system rollbacks like Atomic Distros
+dfwrt com.apple.Finder WarnOnEmptyTrash -bool true # Just to be safe
 
 # Font Smoothing, makes text viewing comfortable
 dfwrt -g CGFontRenderingFontSmoothingDisabled -bool NO
 defaults -currentHost write -globalDomain AppleFontSmoothing -int 3
 
 # Productivity
-dfwrt com.apple.finder "FXEnableExtensionChangeWarning" -bool "false"
+sudo dfwrt /Library/Preferences/com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true # Add them if you want to manually
+dfwrt com.apple.finder "FXEnableExtensionChangeWarning" -bool "false" # Not permanently destructive, no warning required here
+dfwrt com.apple.TextEdit RichText -int 0 # Just use Notion or Obsidian instead of this
 dfwrt -g NSAutomaticSpellingCorrectionEnabled -bool false
 dfwrt NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
 dfwrt NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
@@ -26,16 +28,21 @@ dfwrt com.apple.Finder ShowRemovableMediaOnDesktop -bool true
 dfwrt com.apple.Finder ShowMountedServersOnDesktop -bool true
 dfwrt com.apple.finder "FXEnableExtensionChangeWarning" -bool false
 dfwrt /Library/Preferences/com.apple.mDNSResponder.plist NoMulticastAdvertisements -bool NO
+dfwrt com.apple.finder FXDefaultSearchScope -string "SCcf"
 
 # ON Telemetry
 launchctl load -w /System/Library/LaunchAgents/com.apple.ReportCrash.plist
 sudo launchctl load -w /System/Library/LaunchDaemons/com.apple.ReportCrash.Root.plist
 brew analytics on
-defaults write com.apple.appleseed.FeedbackAssistant Autogather -bool true
+dfwrt com.apple.appleseed.FeedbackAssistant Autogather -bool true
 
 # ON Security
 dfwrt com.apple.LaunchServices LSQuarantine -bool true
-defaults write com.apple.Safari WarnAboutFraudulentWebsites -bool true
+dfwrt com.apple.Safari WarnAboutFraudulentWebsites -bool true
+
+# Ecosystem
+dfwrt com.apple.desktopservices DSDontWriteNetworkStores -bool false
+dfwrt com.apple.desktopservices DSDontWriteUSBStores -bool false
 
 # Energy
 pmset -b sleep 2
