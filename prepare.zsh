b6471be409038a88c429be96f23aa10ae4dcbe0b
Avaneesh Jambhale <90239357+MrGrappleMan@users.noreply.github.com>
2025-09-29 22:34:16 +0530

    Delete prepare.zsh

--- prepare.zsh
+++
@@ -1,6 +0,0 @@
-#!/bin/zsh
-
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew install fish
-echo /opt/homebrew/bin/fish | sudo tee -a /etc/shells
-fish_add_path "/opt/homebrew/bin/"
